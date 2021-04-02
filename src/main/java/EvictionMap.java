1009747823b1c6ed96aed5e2a93f8863a99e4c7b
Lokni <dmitrikalvan@gmail.com>
2021-04-02 12:39:56 +0300

    I get new idee and start made this task in other way

--- src/main/java/EvictionMap.java
+++ src/main/java/EvictionMap.java
@@ -3,49 +3,21 @@ import java.time.temporal.ChronoUnit;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
 
 
 public class EvictionMap<K, V> {
     private final Long expireTimeInMs;
-    private final Map<K, ObjectHolder> storage;
+    private final Map<K, EvictableValueHolder<K,V>> storage;
+    private final ReentrantLock lock;
 
     public EvictionMap(Long expireTimeInMs) {
-        this.storage = new ConcurrentHashMap<>();
         this.expireTimeInMs = expireTimeInMs;
+        this.storage = new ConcurrentHashMap<>();
+        this.lock = new ReentrantLock();
     }
 
-    public void put(K key, V value) {
-        storage.put(key, new ObjectHolder(value, LocalDateTime.now().plus(expireTimeInMs, ChronoUnit.MILLIS)));
+    private static final class EvictableValueHolder<K,V>{
+
     }
-
-    public V get(K key) {
-        removeIfExpired(key);
-        return Optional.ofNullable(storage.get(key))
-                .map(ObjectHolder::getValue)
-                .orElse(null);
-    }
-
-    private void removeIfExpired(K key) {
-        Optional.ofNullable(storage.get(key)).ifPresent(objectHolder -> {
-            if (objectHolder.expirationTime.isBefore(LocalDateTime.now())) {
-                storage.remove(key);
-            }
-        });
-    }
-
-
-    private class ObjectHolder {
-        private final V value;
-        private final LocalDateTime expirationTime;
-
-        protected ObjectHolder(V value, LocalDateTime currentTime) {
-            this.value = value;
-            this.expirationTime = currentTime;
-        }
-
-        public V getValue() {
-            return value;
-        }
-    }
-
 }
