54e9e8f885cef4b2657c54716a3aa94235ce92f8
Lokni <dmitrikalvan@gmail.com>
2021-04-02 12:56:43 +0300

    Change handler class and add interface for time tracking.

--- src/main/java/EvictionMap.java
+++ src/main/java/EvictionMap.java
@@ -1,14 +1,13 @@
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.Map;
-import java.util.Optional;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.ReentrantLock;
 
 
 public class EvictionMap<K, V> {
     private final Long expireTimeInMs;
-    private final Map<K, EvictableValueHolder<K,V>> storage;
+    private final Map<K, EvictableValueHolder<K, V>> storage;
     private final ReentrantLock lock;
 
     public EvictionMap(Long expireTimeInMs) {
@@ -17,7 +16,30 @@ public class EvictionMap<K, V> {
         this.lock = new ReentrantLock();
     }
 
-    private static final class EvictableValueHolder<K,V>{
+    private interface ExpirationHandler<K, V> {
+        void onExpired(K key, EvictableValueHolder<K, V> evictableValueHolder);
+    }
 
+    private static final class EvictableValueHolder<K, V> {
+        private final V value;
+
+        protected EvictableValueHolder(K key,
+                                       V value,
+                                       long expirationDelay,
+                                       EvictionMap.ExpirationHandler<K, V> expirationHandler) {
+            this.value = value;
+
+            new Timer().schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    expirationHandler.onExpired(key, EvictableValueHolder.this);
+                }
+            }, expirationDelay);
+
+        }
+
+        public V getValue() {
+            return value;
+        }
     }
 }
